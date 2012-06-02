9e551cf6de5a54af4af1f4c7cde2b3089d81bfba
Alexander Brock <Brock.Alexander@web.de>
2012-06-02 23:40:55 +0200

    added logging software

---
+++ logger/pachube.sh
@@ -0,0 +1,6 @@
+curl -m 40 --request PUT \
+     --data-binary @pachube.json \
+     --header "X-PachubeApiKey: 0VjJTr86Ncb3hXMvVT8-Wq4gk-KSAKx5NXVhanZDVUYrQT0g" \
+     --verbose \
+     http://api.pachube.com/v2/feeds/42055 &
+
