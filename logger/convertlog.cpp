bb7837cef1675f32b52095666ea7991b741b02af
Alexander Brock <Brock.Alexander@web.de>
2012-06-04 17:26:39 +0200

    added tool to import old logs into new database

---
+++ logger/convertlog.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void trim(string& s) {
+	size_t pos = 0;
+	while (s.size() > pos && (s[pos] == ' ' || s[pos] == '\r' || s[pos] == '\n' || s[pos] == '\t')) {
+		pos++;
+	}
+	s = s.erase(0, pos);
+}
+
+int main(void) {
+	const size_t buf_size = 1024*1024;
+	char buf[buf_size];
+	while (!cin.eof()) {
+		cin.getline(buf, buf_size);
+		string line(buf);
+		trim(line);
+		if (line.length() > 0) {
+			cout << "put electricity.consumption " << line << " location=RZL" << endl;
+		}
+	}
+}
